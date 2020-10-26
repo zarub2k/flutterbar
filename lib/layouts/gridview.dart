d45746860e1014c6376bd697305cc5a709edde71
Tham <zarub2k@gmail.com>
2020-10-26 11:49:46 +0530

    Grid layout box layout tried

--- lib/layouts/gridview.dart
+++ lib/layouts/gridview.dart
@@ -8,12 +8,19 @@ class GridViewLayout extends StatelessWidget {
 
   GridView _build(BuildContext context) {
     return GridView.count(
-      crossAxisCount: 3,
-      children: _buildGridTiles(6),
+      crossAxisCount: 2,
+      children: _buildGridTiles(20),
     );
   }
 
   List<Container> _buildGridTiles(int length) {
-    return List.generate(length, (index) => Container(child: Text('G$index'),));
+    return List.generate(length, (index) =>
+      Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.amber),
+          shape: BoxShape.rectangle
+        ),
+        child: Center(child: Text('G${index+1}'),))
+    );
   }
 }
