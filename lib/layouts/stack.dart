e71939df032394cf41a4fd486b8028f49e9336be
Tham <zarub2k@gmail.com>
2020-10-27 09:27:38 +0530

    Stack layout work started

---
+++ lib/layouts/stack.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class StackLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _build(context);
+  }
+
+  Stack _build(BuildContext context) {
+    return Stack(
+
+    );
+  }
+}
