dfba220150e2174aa171b15c602880020ab6842f
Tham <zarub2k@gmail.com>
2020-11-01 22:24:42 +0530

    lake image added

---
+++ lib/layouts/layout01.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Layout01 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _build(context);
+  }
+  
+  Widget _build(BuildContext context) {
+    return null; 
+  }
+}
