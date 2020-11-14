a98e95a7228da894dcca0e33bd76624f6ebb76d0
Tham <zarub2k@gmail.com>
2020-11-14 07:41:09 +0530

    constraint work started

---
+++ lib/layouts/constraint.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Constraints extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  _build(context);
+  }
+  
+  Widget _build(BuildContext context) {
+    return null;
+  }
+}
